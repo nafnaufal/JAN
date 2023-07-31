ffec861cbed42a1fe1c084fb6aeb82b3c6c1b361
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2023-08-01 01:47:39 +0700

    update
    resolve route error in guru
    adding logo in sidebar

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller
+class Guru extends CI_Controller
 {
 
     public function index()
--- application/views/templates_guru/header.php
+++ application/views/templates_guru/header.php
@@ -98,7 +98,7 @@
               </a>
             </li>
             <li class="menu-item">
-              <a href="<?php echo base_url(); ?>guru/data_guru" class="menu-link">
+              <a href="<?php echo base_url(); ?>guru/nilai" class="menu-link">
                 <i class="bi bi-mortarboard-fill mx-1"></i> 
                 <div data-i18n="Analytics">Data Nilai</div>
               </a>
