7f129f0f4a0dd01c24c22af129edd76d37d1d6a0
nafnaufal <naufalanbial@gmail.com>
2023-09-15 07:08:10 +0700

    jadwal

---
+++ application/views/wali_murid/jadwal/edit.php
@@ -0,0 +1,144 @@
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pendaftaran /</span> Edit</h4>
+
+        <!-- Basic Layout & Basic with Icons -->
+        <div class="row">
+            <!-- Basic Layout -->
+            <div class="col-xxl">
+                <div class="card mb-4">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                        <h5 class="mb-0">Pendaftaran</h5>
+                        <!-- <small class="text-muted float-end">Add</small> -->
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="<?= base_url() ?>wali/pendaftaran/update?id=<?= $data->id ?>" onsubmit="submitItem(event)">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="anak">Nama Anak</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="anak" name="anak" value="<?= $data->nama ?>" placeholder="Nama" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="username">Username</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="username" name="username" value="<?= $data->username ?>" placeholder="Username" readonly />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="tanggal_lahir">Tanggal Lahir</label>
+                                <div class="col-sm-10">
+                                    <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="<?= $data->tanggal_lahir ?>" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="kelamin">Jenis Kelamin</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="kelamin" name="jk" aria-label="Jenis Kelamin">
+                                        <option value="Laki-Laki" <?php if ($data->jk === "Laki-Laki") {
+                                                                        echo "selected";
+                                                                    } ?>>Laki-Laki</option>
+                                        <option value="Perempuan" <?php if ($data->jk === "Perempuan") {
+                                                                        echo "selected";
+                                                                    } ?>>Perempuan</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="ayah">Nama Ayah</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="ayah" name="ayah" value="<?= $data->nama_ayah ?>" placeholder="Nama Ayah" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="ibu">Nama Ibu</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="ibu" name="ibu" value="<?= $data->nama_ibu ?>" placeholder="Nama Ibu" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="agama">Agama</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="agama" name="agama" aria-label="Agama">
+                                        <option value="Islam" <?php if ($data->agama === "Islam") {
+                                                                    echo "selected";
+                                                                } ?>>Islam</option>
+                                        <option value="Katolik" <?php if ($data->agama === "Katolik") {
+                                                                    echo "selected";
+                                                                } ?>>Katolik</option>
+                                        <option value="Protestan" <?php if ($data->agama === "Protestan") {
+                                                                        echo "selected";
+                                                                    } ?>>Protestan</option>
+                                        <option value="Hindu" <?php if ($data->agama === "Hindu") {
+                                                                    echo "selected";
+                                                                } ?>>Hindu</option>
+                                        <option value="Budha" <?php if ($data->agama === "Budha") {
+                                                                    echo "selected";
+                                                                } ?>>Budha</option>
+                                        <option value="Konghucu" <?php if ($data->agama === "Konghucu") {
+                                                                        echo "selected";
+                                                                    } ?>>Konghucu</option>
+                                        <option value="Lainnya" <?php if ($data->agama === "Lainnya") {
+                                                                    echo "selected";
+                                                                } ?>>Lainnya</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="alamat">Alamat</label>
+                                <div class="col-sm-10">
+                                    <textarea id="alamat" class="form-control" name="alamat" placeholder="Alamat"><?= $data->alamat ?></textarea>
+                                </div>
+                            </div>
+                            <div class="row justify-content-end">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Edit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Content -->
+
+    <script>
+        function submitItem(event) {
+            if (ageCalculator() < 5) {
+                event.preventDefault();
+
+                Swal.fire({
+                    title: 'Usia Anak Belum 5 Tahun!',
+                    icon: 'warning',
+                });
+            } else {
+                Swal.fire({
+                    title: 'Data Tersimpan',
+                    icon: 'success',
+                });
+            }
+        }
+
+        function ageCalculator() {
+            var userinput = document.getElementById("tanggal_lahir").value;
+            var dob = new Date(userinput);
+
+            //calculate month difference from current date in time  
+            var month_diff = Date.now() - dob.getTime();
+
+            //convert the calculated difference in date format  
+            var age_dt = new Date(month_diff);
+
+            //extract year from date      
+            var year = age_dt.getUTCFullYear();
+
+            //now calculate the age of the user  
+            var age = Math.abs(year - 1970);
+
+            //display the calculated age  
+            return age;
+        }
+    </script>
+</div>
---
+++ application/views/wali_murid/jadwal/view.php
@@ -0,0 +1,55 @@
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Jadwal /</span> Detail</h4>
+        <a href="<?php echo base_url(); ?>wali/pendaftaran/edit" class="btn btn-primary mb-2">Edit</a>
+
+        <!-- Basic Layout & Basic with Icons -->
+        <div class="row">
+            <!-- Basic Layout -->
+            <div class="col-xxl">
+                <div class="card mb-4">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                        <h5 class="mb-0">Jadwal</h5>
+                        <!-- <small class="text-muted float-end">Add</small> -->
+                    </div>
+                    <div class="card-body">
+                        <div class="card mb-4">
+                            <div class="table-responsive text-nowrap">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Tanggal / Waktu</th>
+                                            <th>Mata Pelajaran</th>
+                                            <th>Ruangan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="table-border-bottom-0">
+                                        <tr>
+                                            <td>19-2-2020 / 19.10 WIB</td>
+                                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
+                                            <td><span class="badge bg-label-primary me-1">1 A</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td>19-2-2020 / 19.10 WIB</td>
+                                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
+                                            <td><span class="badge bg-label-primary me-1">1 A</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td>19-2-2020 / 19.10 WIB</td>
+                                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
+                                            <td><span class="badge bg-label-primary me-1">1 A</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Content -->
+
+
+</div>
