84a77c87f67789567a53a9490ff19eac7f62c758
nafnaufal <naufalanbial@gmail.com>
2023-09-15 07:27:09 +0700

    jadwal views

---
+++ application/views/wali_murid/jadwal/add.php
@@ -0,0 +1,144 @@
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pendaftaran /</span> Add</h4>
+
+        <!-- Basic Layout & Basic with Icons -->
+        <div class="row">
+            <!-- Basic Layout -->
+            <div class="col-xxl">
+                <div class="card mb-4">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                        <h5 class="mb-0">Jadwal</h5>
+                        <!-- <small class="text-muted float-end">Add</small> -->
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="<?= base_url() ?>wali/pendaftaran/update?id=<?= $data->id ?>" onsubmit="submitItem(event)">
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="tanggal">Tanggal</label>
+                                <div class="col-sm-10">
+                                    <input type="date" class="form-control" id="tanggal" name="tanggal" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="mulai">Jam Mulai</label>
+                                <div class="col-sm-10">
+                                    <input type="time" class="form-control" id="mulai" name="mulai" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="selesai">Jam Selesai</label>
+                                <div class="col-sm-10">
+                                    <input type="time" class="form-control" id="selesai" name="selesai" />
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="mapel">Mata Pelajaran</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="mapel" name="mapel" aria-label="mapel">
+                                        <option value="Berhitung" <?php
+                                                                    // if ($data->jk === "Laki-Laki") {
+                                                                    //     echo "selected";
+                                                                    // }
+                                                                    ?>>Berhitung</option>
+                                        <option value="Menulis" <?php
+                                                                // if ($data->jk === "Laki-Laki") {
+                                                                //     echo "selected";
+                                                                // }
+                                                                ?>>Menulis</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="ruang">Ruangan</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="ruang" name="ruang" aria-label="ruang">
+                                        <option value="1 A" <?php
+                                                            // if ($data->jk === "Laki-Laki") {
+                                                            //     echo "selected";
+                                                            // }
+                                                            ?>>1 A</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="guru">Guru</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="guru" name="guru" aria-label="guru">
+                                        <option value="Guru A" <?php
+                                                                // if ($data->jk === "Laki-Laki") {
+                                                                //     echo "selected";
+                                                                // }
+                                                                ?>>Guru A</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-2 col-form-label" for="mapel">Mata Pelajaran</label>
+                                <div class="col-sm-10">
+                                    <select class="form-select" id="mapel" name="mapel" aria-label="mapel">
+                                        <option value="Berhitung" <?php
+                                                                    // if ($data->jk === "Laki-Laki") {
+                                                                    //     echo "selected";
+                                                                    // }
+                                                                    ?>>Berhitung</option>
+                                        <option value="Menulis" <?php
+                                                                // if ($data->jk === "Laki-Laki") {
+                                                                //     echo "selected";
+                                                                // }
+                                                                ?>>Menulis</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row justify-content-end">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Edit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Content -->
+
+    <script>
+        function submitItem(event) {
+            if (ageCalculator() < 5) {
+                event.preventDefault();
+
+                Swal.fire({
+                    title: 'Usia Anak Belum 5 Tahun!',
+                    icon: 'warning',
+                });
+            } else {
+                Swal.fire({
+                    title: 'Data Tersimpan',
+                    icon: 'success',
+                });
+            }
+        }
+
+        function ageCalculator() {
+            var userinput = document.getElementById("tanggal_lahir").value;
+            var dob = new Date(userinput);
+
+            //calculate month difference from current date in time  
+            var month_diff = Date.now() - dob.getTime();
+
+            //convert the calculated difference in date format  
+            var age_dt = new Date(month_diff);
+
+            //extract year from date      
+            var year = age_dt.getUTCFullYear();
+
+            //now calculate the age of the user  
+            var age = Math.abs(year - 1970);
+
+            //display the calculated age  
+            return age;
+        }
+    </script>
+</div>
--- application/views/wali_murid/jadwal/view.php
+++ application/views/wali_murid/jadwal/view.php
@@ -2,7 +2,7 @@
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Jadwal /</span> Detail</h4>
-        <a href="<?php echo base_url(); ?>wali/pendaftaran/edit" class="btn btn-primary mb-2">Edit</a>
+        <a href="<?php echo base_url(); ?>wali/jadwal/add" class="btn btn-primary mb-2">Add jadwal</a>
 
         <!-- Basic Layout & Basic with Icons -->
         <div class="row">
@@ -16,29 +16,24 @@
                     <div class="card-body">
                         <div class="card mb-4">
                             <div class="table-responsive text-nowrap">
+                                <p class="mb-0"><strong>09-2-2020</strong></p>
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th>Tanggal / Waktu</th>
+                                            <th>Waktu Mulai</th>
+                                            <th>Waktu Selesai</th>
                                             <th>Mata Pelajaran</th>
                                             <th>Ruangan</th>
+                                            <th>Guru</th>
                                         </tr>
                                     </thead>
                                     <tbody class="table-border-bottom-0">
                                         <tr>
-                                            <td>19-2-2020 / 19.10 WIB</td>
+                                            <td>9.10 WIB</td>
+                                            <td>10.10 WIB</td>
                                             <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
                                             <td><span class="badge bg-label-primary me-1">1 A</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>19-2-2020 / 19.10 WIB</td>
-                                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
-                                            <td><span class="badge bg-label-primary me-1">1 A</span></td>
-                                        </tr>
-                                        <tr>
-                                            <td>19-2-2020 / 19.10 WIB</td>
-                                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Matematika</strong></td>
-                                            <td><span class="badge bg-label-primary me-1">1 A</span></td>
+                                            <td>Guru A</td>
                                         </tr>
                                     </tbody>
                                 </table>
